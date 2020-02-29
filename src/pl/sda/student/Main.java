bbfbb47b23f502e3844c8c0b86b137db985d3062
Justyna <daszkowskajustyna@gmail.com>
2020-02-29 11:49:09 +0100

    zapisywanie obiektu do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -11,11 +11,21 @@ public class Main {
 	// write your code here
 
         Student student = new Student("Jan", "Kowalski", 32 );
+        Student student1 = new Student ("Piotr", "Lis", 21);
+        Student student2 = new Student("Paweł", "Nowak", 23);
+        Student student3 = new Student("Tomasz", "Wilk", 40);
+        Student student4 = new Student("Zbigniew", "Gawron",60);
+        Student student5 = new Student("Aleksander", "Kwaśnik" ,53);
 
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
             objectMapper.writeValue (new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student.json"), student4);
+            objectMapper.writeValue(new File("student.json"), student5);
         } catch (IOException e) {
             e.printStackTrace();
         }
